ea3c14a670a37cad00ef91480894c00a90eae5c5
Alejandro Chazarreta <lcrender@gmail.com>
2023-03-01 03:54:00 +0100

    update

--- server/app/routes/rooms.routes.js
+++ server/app/routes/rooms.routes.js
@@ -1,12 +1,8 @@
 const {Router}  = require('express');
 const router = Router();
 const { 
-    renderRoomForm,
     createNewRoom,
     renderRooms,
-    renderEditForm,
-    updateRoom,
-    deleteRoom,
     enterRoom  } = require('../controllers/rooms.controllers')
 const verifyToken = require('../middlewares/verifyToken');
 
@@ -16,14 +12,17 @@ router.post('/rooms/add',verifyToken, createNewRoom)
 // View all Rooms
 router.get('/rooms', renderRooms)
 
+// Enter in a Room
+router.put('/rooms/enter', enterRoom)
+
+
+// API V2
 // Edit Rooms
-// router.get('/rooms/edit/:id', renderEditForm)
 // router.put('/rooms/edit/:id', updateRoom)
 
 // Delete Room
 // router.delete('/rooms/delete/:id',verifyToken, deleteRoom)
 
-// Enter in a Room
-router.put('/rooms/enter', enterRoom)
+
 
 module.exports = router
